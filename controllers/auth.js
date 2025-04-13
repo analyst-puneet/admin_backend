e3f40e0976f2c14244a00c75a09040a0896121d3
hemant4556 <Hemantkumar45506@gmail.com>
2025-04-13 17:33:13 +0530

    Hemant

--- controllers/auth.js
+++ controllers/auth.js
@@ -63,4 +63,26 @@ const logout = (req, res) => {
     res.clearCookie("remember_token");
     res.json({ message: "Logged out successfully" });
 };
-module.exports = {  login, logout};
+const validate = async (req, res) => {
+    try {
+        const token = req.cookies.token; 
+        const remember_token = req.cookies.remember_token; 
+        const userId = req.cookies.UserId; 
+        if (!remember_token || !userId || !token) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+
+        const user = await User.findOne({ _id: userId });
+
+        if (user) {
+            return res.status(200).json({ userId: user._id });
+        } else {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+    } catch (error) {
+        console.error("Validation error:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+module.exports = { login, logout, validate };
--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const connectDB = require('./config/database'); 
+const cors = require("cors");
+const cookieParser = require('cookie-parser');
 connectDB();
 
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 const authRoutes = require('./routes/auth');
+const corsOption = {
+    origin: ['http://localhost:5173'],
+    credentials: true,
+};
+app.use(cors(corsOption));
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
