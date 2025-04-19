dcbf75541ebc69235419c5805a2e32defc1ce832
hemant4556 <Hemantkumar45506@gmail.com>
2025-04-19 15:47:40 +0530

    hemant

--- routes/MasterRoutes.js
+++ routes/MasterRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const MasterModel = require('../models/MasterModel');
 const { Index, Create, Update, Delete } = require('../controllers/MasterData');
 
-const validMasters = ['role_group', 'Country', 'state', 'gender', 'blood_group'];
+const validMasters = ['role_group', 'ountry', 'state', 'gender', 'blood_group'];
 
 router.use('/:masterName', (req, res, next) => {
   const { masterName } = req.params;
